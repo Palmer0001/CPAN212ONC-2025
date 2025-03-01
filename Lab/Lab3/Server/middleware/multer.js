52d73ae607ea1bb7352b47139ebc6eec5ac743d1
Palmer0001 <palmer.pa123@gmail.com>
2025-02-28 23:23:32 -0500

    updates

--- Lab/Lab3/Server/middleware/multer.js
+++ Lab/Lab3/Server/middleware/multer.js
@@ -6,13 +6,14 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const storage = multer.diskStorage({
-
+  
   destination: function (req, file, cb) {
     
     cb(null, path.join(__dirname, "../uploads"));
   },
   filename: function (req, file, cb) {
-    cb(null, `${Date.now()}-${file.originalname}`);nsion
+    
+    cb(null, `${Date.now()}-${file.originalname}`); 
   },
 });
 
