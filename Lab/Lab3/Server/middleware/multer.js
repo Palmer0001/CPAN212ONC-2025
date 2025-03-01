1f6246b0bdd0c64e8ae1a494f5387615d225875f
Palmer0001 <palmer.pa123@gmail.com>
2025-02-28 23:17:31 -0500

    updates

--- Lab/Lab3/Server/middleware/multer.js
+++ Lab/Lab3/Server/middleware/multer.js
@@ -6,17 +6,16 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const storage = multer.diskStorage({
-  // the function that saves the file
+
   destination: function (req, file, cb) {
-    // where we are storing the file
+    
     cb(null, path.join(__dirname, "../uploads"));
   },
   filename: function (req, file, cb) {
-    // what is the filename going to be
-    cb(null, `${Date.now()}-${file.originalname}`); // this will save files like: date-filename.extension
+    cb(null, `${Date.now()}-${file.originalname}`);nsion
   },
 });
 
-const upload = multer({ storage: storage }); // the middleware function that handles uploading
+const upload = multer({ storage: storage }); 
 
 export default upload;
